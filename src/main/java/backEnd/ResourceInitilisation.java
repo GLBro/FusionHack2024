953b048d680f3a10c11237ef4daa58703170da91
Lauren Field <lf4g23@soton.ac.uk>
2024-03-16 14:28:29 +0000

    basic set up for ResourceInitilisation

--- src/main/java/backEnd/ResourceInitilisation.java
+++ src/main/java/backEnd/ResourceInitilisation.java
@@ -5,5 +5,13 @@ import java.util.ArrayList;
 public class ResourceInitilisation {
   private ArrayList<Resource> resources;
 
+  public ResourceInitilisation(String fileName){
+
+  }
+
+  public Resource fromName(){
+
+    return resources.get(0);
+  }
 
 }
