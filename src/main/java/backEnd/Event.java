831f2f6bbe40d22e3cd9217fd7dd7360134e2f48
axtan64 <pigeon7846@gmail.com>
2024-03-16 13:59:29 +0000

    Event skeleton

--- src/main/java/backEnd/Event.java
+++ src/main/java/backEnd/Event.java
@@ -1,9 +1,21 @@
 package backEnd;
 
+import java.util.ArrayList;
+
 public class Event {
   private String name;
+  private ArrayList<Resource> resourcesAffected;
 
-  public void setName(String name) {
+  public Event(String name) {
     this.name = name;
+  }
+
+  public Event(String name, ArrayList<Resource> resourcesAffected) {
+    this(name);
+    this.resourcesAffected = resourcesAffected;
+  }
+
+  public void addResource(Resource resource) {
+    this.resourcesAffected.add(resource);
   }
 }
