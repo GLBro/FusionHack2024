b5259896947e1dbf818ca570f160435bde5bf4f1
Lauren Field <lf4g23@soton.ac.uk>
2024-03-16 13:48:04 +0000

    Made EventPicker

--- src/main/java/backEnd/EventPicker.java
+++ src/main/java/backEnd/EventPicker.java
@@ -1,4 +1,15 @@
 package backEnd;
 
+import java.util.ArrayList;
+
 public class EventPicker {
+  private ArrayList<Event> events;
+  public EventPicker(String file){
+    initialiseArray(file);
+  }
+
+  public ArrayList<Event> initialiseArray(String FileName){
+
+    return events;
+  };
 }
