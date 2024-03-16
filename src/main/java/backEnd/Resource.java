02ad00b4425e667ac8b9f238c2db31bce92ad483
Lauren Field <lf4g23@soton.ac.uk>
2024-03-16 13:27:41 +0000

    Initialisation of Resource

--- src/main/java/backEnd/Resource.java
+++ src/main/java/backEnd/Resource.java
@@ -1,4 +1,13 @@
 package backEnd;
 
 public class Resource {
+  private String name;
+  private double cost;
+  private double lastCost;
+
+  public Resource(String name){
+    this.name = name;
+    this.cost = 0.00;
+  }
+
 }
