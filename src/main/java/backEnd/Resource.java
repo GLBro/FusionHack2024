cebfe1926ba887f5a1b081ac0bd441fac4ecec3b
Lauren Field <lf4g23@soton.ac.uk>
2024-03-16 13:30:37 +0000

    Somthings gone wrong

--- src/main/java/backEnd/Resource.java
+++ src/main/java/backEnd/Resource.java
@@ -1,13 +1,22 @@
 package backEnd;
-
 public class Resource {
   private String name;
   private double cost;
   private double lastCost;
 
+
   public Resource(String name){
     this.name = name;
     this.cost = 0.00;
+  }
+
+  public void setLastCost() {
+
+    this.lastCost = this.cost;
+  }
+
+  public void setCost(double cost) {
+    this.cost = cost;
   }
 
 }
