d06e81710cbf3318416edace4ef17c95a0be931f
axtan64 <pigeon7846@gmail.com>
2024-03-16 14:20:47 +0000

    initialiseArray skeleton

--- src/main/java/backEnd/EventPicker.java
+++ src/main/java/backEnd/EventPicker.java
@@ -1,5 +1,8 @@
 package backEnd;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.util.ArrayList;
 
 public class EventPicker {
@@ -8,8 +11,23 @@ public class EventPicker {
     initialiseArray(file);
   }
 
-  public ArrayList<Event> initialiseArray(String FileName){
+  public ArrayList<Event> initialiseArray(String fileName) {
+    try {
+      File file = new File(fileName);
+      BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
 
+      String line;
+      while((line = bufferedReader.readLine()) != null) {
+        String[] splitLine = line.split(",");
+        events.add(new Event(
+            splitLine[0]
+        ));
+      }
+
+      bufferedReader.close();
+    } catch (Exception exception) {
+      System.out.println("Error");
+    }
     return events;
   };
 
