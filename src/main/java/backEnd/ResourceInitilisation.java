51e2fae2e6cf46587c3a7439c302082ff5f08c17
Lauren Field <lf4g23@soton.ac.uk>
2024-03-16 14:36:02 +0000

    string name to class

--- src/main/java/backEnd/ResourceInitilisation.java
+++ src/main/java/backEnd/ResourceInitilisation.java
@@ -9,9 +9,14 @@ public class ResourceInitilisation {
 
   }
 
-  public Resource fromName(){
+  public Resource fromName(String name){
+    for(int i=0; i< resources.size();i++){
+      if (resources.get(i).getName() == name){
+        return resources.get(i);
+      }
+    }
 
-    return resources.get(0);
+    return null;
   }
 
 }
